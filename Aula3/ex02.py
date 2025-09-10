5b5bb9c37e6188dadc182eb146154b70d5009cfd
Renan Paulino <dev.renanpaulino@gmail.com>
2025-09-09 22:37:48 -0300

    Add files via upload

---
+++ Aula3/ex02.py
@@ -0,0 +1,15 @@
+print("Calculadora de peso ideal")
+
+altura = int(input("Digite sua altura em metros: "))
+peso = float(input("Digite o seu peso: "))
+
+imc = peso / (peso * altura)
+
+if (imc < 20):
+    print("Abaixo do peso")
+elif (imc < 25):
+    print("Peso Ideal!")
+else:
+    print("Acima do Peso!")
+
+print("Fim Programa!")
---
+++ Aula3/exercicio8.py
@@ -0,0 +1,9 @@
+v1 = float(input("Entre com ou valor: "))
+v2 = float(input("Entre com outro valor: "))
+
+if (v1 > v2):
+    print(f"O maior valor é {v1}")
+else:
+    print(f"O maior valor é {v2}")
+
+
