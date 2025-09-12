90623dcce87e899991b3278898c11e26a364e62f
Renan Paulino <dev.renanpaulino@gmail.com>
2025-09-11 21:33:50 -0300

    Add files via upload

---
+++ Aula1eAula2-Python/Exemplo.py
@@ -0,0 +1,13 @@
+print ("Hello, World!")
+
+nome = str(input("Digite o nome do aluno: "))
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+
+media = (nota1 + nota2) / 2
+print("A média é:", media)
+
+if media <= 6:
+  print("Reprovado")
+else:
+  print("Aprovado")
